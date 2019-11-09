cc37ae94526ae18cb6811f6450831236677c1c7d
David Emily <daeth5@mail.missouri.edu>
2019-11-09 16:32:40 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+let grpc = require("grpc");
+var protoLoader = require("@grpc/proto-loader");
+
+const server = new grpc.Server();
+const SERVER_ADDRESS = "0.0.0.0:5001";
+
+//Load protobuf
+let proto = grpc.loadPackageDefinition(
+  protoLoader.loadSync("protos/chat.proto", {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  })
+);
+
+let users = [];
+
+function join(call, callback) {
+  users.push(call);
+  notifyChat({ user: "Server", text: "New user joined " });
+}
+
+function send(call, callback) {
+  notifyChat(call.request);
+}
+
+function notifyChat(message) {
+  users.forEach(user => {
+    user.write(message);
+  });
+}
+
+server.addService(proto.example.Chat.service, { join: join, send: send });
+
+server.bind(SERVER_ADDRESS, grpc.ServerCredentials.createInsecure());
+
+server.start();
+
+console.log(`"Server started on ${SERVER_ADDRESS}"`);
